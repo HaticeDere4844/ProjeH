c800aff1f989c9d66e08e42647e26d1e6dc37fd0
31635 <haticedere4844@gmail.com>
2023-01-31 17:39:28 +0100

    task 2 yapildi

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -4,6 +4,9 @@ public class Ogrenci {
     public static void main(String[] args) {
 
         System.out.println(" Hi... ");
+        int sayi1=7;
+        int sayi2=9;
+        System.out.println("toplam = " + sayi1+sayi2);
 
     }
 }
